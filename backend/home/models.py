55305c19cfd6d2d55a5629cc833a013735e5d5b9
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-17 17:24:59 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,9 @@
+from django.conf import settings
 from django.db import models
+class Pet(models.Model):
+    'Generated Model'
+    name = models.TextField()
+    type = models.TextField()
+    image_url = models.URLField()
+    description = models.TextField()
+    attributes = models.TextField()
