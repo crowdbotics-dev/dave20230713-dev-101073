1d86f032129d30ca455860418f4b16b2d2e77773
Dave.o Olund <dave.o@crowdbotics.com>
2023-07-19 17:22:03 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "dave20230713-dev-101073",
-  url: "https://dave20230713-dev-101073.botics.co",
-  api: "https://dave20230713-dev-101073.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "dave20230713-dev-101073",
+  "url": "https://dave20230713-dev-101073.botics.co",
+  "api": "https://dave20230713-dev-101073.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "davewelcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
