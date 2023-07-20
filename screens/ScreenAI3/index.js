e4f670140dbc103f3f65117e3b2869483f006351
Dave.o Olund <dave.o@crowdbotics.com>
2023-07-20 13:27:29 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI3/index.js
+++ screens/ScreenAI3/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, SafeAreaView } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Avatar, Card, Title, Paragraph } from "react-native-paper";
 
 const ProfileScreen = () => {
   return <SafeAreaView style={styles.container}>
       <View style={styles.profileContainer}>
-        <Image source={{
-        uri: 'https://tinyurl.com/42evm3m3'
-      }} style={styles.profileImage} />
-        <Text style={styles.name}>John Doe</Text>
-        <Text style={styles.bio}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae justo ac nunc tincidunt aliquet. Fusce auctor, nunc id aliquam tincidunt, nunc nunc lacinia nunc, id lacinia nunc nunc id nunc.</Text>
+        <Avatar.Image source={{
+        uri: "https://tinyurl.com/42evm3m3"
+      }} size={150} />
+        <Card>
+          <Card.Content>
+            <Title style={styles.name}>John Doe</Title>
+            <Paragraph style={styles.bio}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae
+              justo ac nunc tincidunt aliquet. Fusce auctor, nunc id aliquam
+              tincidunt, nunc nunc lacinia nunc, id lacinia nunc nunc id nunc.
+            </Paragraph>
+          </Card.Content>
+        </Card>
       </View>
     </SafeAreaView>;
 };
@@ -16,29 +26,23 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
   profileContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20
-  },
-  profileImage: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-    marginBottom: 20
   },
   name: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10
   },
   bio: {
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
     marginHorizontal: 20
   }
 });
